d039bece579a09ed15bf04e0d0f808ae0a711720
Omprakash Gupta <gomprakashgupta66@gmail.com>
2025-06-24 12:13:19 +0530

    Gmail

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -15,9 +15,9 @@ const syncUserCreation = inngest.createFunction(
         const { id, first_name, last_name, email_addresses, image_url } = event.data
         const userData = {
             _id: id,
-            email: email_addresses[0].email_address,
-            name: first_name + ' ' + last_name,
-            image: image_url
+            _email: email_addresses[0].email_address,
+            _name: first_name + ' ' + last_name,
+            _image: image_url
         }
         await User.create(userData)
     }
@@ -42,9 +42,9 @@ const syncUserUpdation = inngest.createFunction(
         const { id, first_name, last_name, email_addresses, image_url } = event.data
         const userData = {
             _id: id,
-            email: email_addresses[0].email_address,
-            name: first_name + ' ' + last_name,
-            image: image_url
+            _email: email_addresses[0].email_address,
+            _name: first_name + ' ' + last_name,
+            _image: image_url
         }
         await User.findByIdAndUpdate(id, userData)
     }
