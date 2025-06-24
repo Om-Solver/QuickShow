72e00bec199a83a128fec8630d4458b2a71e113b
Omprakash Gupta <gomprakashgupta66@gmail.com>
2025-06-24 12:37:26 +0530

    hypen

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -105,7 +105,7 @@ const sendBookingConfirmationEmail = inngest.createFunction(
                 <p>Your booking for <strong style="color: #F84565;">"${booking.show.movie.title}"</strong> is confirmed.</p>
                 <p>
                     <strong>Date:</strong> ${new Date(booking.show.showDateTime).toLocaleDateString('en-US', { timeZone: 'Asia/Kolkata' })}<br />
-                    <strong>Time:</strong> ${new Date(booking.show.showDateTime).toLocaleTimeString('en_US', { timeZone: 'Asia/Kolkata' })}
+                    <strong>Time:</strong> ${new Date(booking.show.showDateTime).toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata' })}
                 </p>
                 <p>Enjoy the show! 🍿</p>
                 <p>Thanks for booking with us!<br />- QuickShow Team</p>
